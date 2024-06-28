f952d48e0cffdd5389bcd6179c7020f3ec1fa8e7
Gokai9 <tasim.dev@gmail.com>
2024-06-28 19:49:03 +0700

    hobby

---
+++ springapi/src/main/java/com/example/springapi/EmployeeController.java
@@ -0,0 +1,47 @@
+package com.example.springapi;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+    private final EmployeeRepository repository;
+
+    EmployeeController(EmployeeRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/employees")
+    List<Employee> all() {
+        return repository.findAll();
+    }
+    @PostMapping("/employees")
+    Employee postNewEmployee(@RequestBody Employee newEmployee) {
+        return repository.save(newEmployee);
+    }
+    @GetMapping("/employees/{id}")
+    Employee getEmployee(@PathVariable Long id) {
+        return repository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
+    }
+    @PutMapping("/employees/{id}")
+    Employee updateEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
+        return repository.findById(id).map((employee) -> {
+            employee.setName(newEmployee.getName());
+            employee.setRole(newEmployee.getRole());
+            return repository.save(employee);
+        }).orElseGet(() -> {
+            return repository.save(newEmployee);
+        });
+    }
+    @DeleteMapping("/employess/{id}")
+    void deleteEmployee(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+}
